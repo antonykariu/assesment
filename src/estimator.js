02fbaccf23ef98215c650f571b2fb4efeb89cd1f
antonykariu <kariuantony@gmail.com>
2020-04-08 12:29:25 +0300

    Identation errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,14 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
   const impacts = [10, 50];
-  const outcome = (data, impact) => {
-    const currentlyInfected = impact * data.reportedCases;
-    const infectionsByRequestedTime =
-      currentlyInfected * 2 ** (data.timeToElapse / 3);
-    const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
-    const hospitalBedsByByRequestedTime =
-      data.totalHospitalBeds * 0.35 - severeCasesByRequestedTime;
-    const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
-    const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
-    const dollarsInFlight =
-      infectionsByRequestedTime *
-      data.region.avgDailyIncomePopulation *
-      data.region.avgDailyIncomeInUSD *
-      data.timeToElapse;
+  const outcome = (d, impact) => {
+    const currentlyInfected=impact * d.reportedCases;
+    const infectionsByRequestedTime=currentlyInfected * 2 ** (d.timeToElapse / 3);
+    const severeCasesByRequestedTime=infectionsByRequestedTime * 0.15;
+    const hospitalBedsByByRequestedTime=d.totalHospitalBeds * 0.35 - severeCasesByRequestedTime;
+    const casesForICUByRequestedTime=infectionsByRequestedTime * 0.05;
+    const casesForVentilatorsByRequestedTime=infectionsByRequestedTime * 0.02;
+    const dollarsInFlight =infectionsByRequestedTime * d.region.avgDailyIncomePopulation * d.region.avgDailyIncomeInUSD * d.timeToElapse;
 
     return {
       currentlyInfected,

1d9d6167211f5fc5f4e45700ad6efac4edf2d6be
antonykariu <kariuantony@gmail.com>
2020-04-08 13:52:03 +0300

    added week and month conversion

--- src/estimator.js
+++ src/estimator.js
@@ -2,14 +2,18 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const impacts = [10, 50];
   const outcome = (d, impact) => {
+    const week = (d.periodType === "weeks") ? d.timeToElapse * 7 : null;
+    const month = (d.periodType === "months") ? d.timeToElapse * 30 : null;
+    const timeInDays = week ? (month == null) : d.timeToElapse;
+
     const currentlyInfected = impact * d.reportedCases;
-    const infectionsByRequestedTime = currentlyInfected * 2 ** (d.timeToElapse / 3);
+    const infectionsByRequestedTime = currentlyInfected * 2 ** (timeInDays / 3);
     const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
     const hospitalBedsByByRequestedTime = d.totalHospitalBeds * 0.35 - severeCasesByRequestedTime;
     const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
     const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
     const dollarsInFlight = infectionsByRequestedTime * d.region.avgDailyIncomePopulation
-    * d.region.avgDailyIncomeInUSD * d.timeToElapse;
+    * d.region.avgDailyIncomeInUSD * timeInDays;
 
     return {
       currentlyInfected,

390b51eb73555260a6adbe8df097d930d4054bb5
antonykariu <kariuantony@gmail.com>
2020-04-08 12:33:04 +0300

    linting errors fix

--- src/estimator.js
+++ src/estimator.js
@@ -2,13 +2,13 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const impacts = [10, 50];
   const outcome = (d, impact) => {
-    const currentlyInfected=impact * d.reportedCases;
-    const infectionsByRequestedTime=currentlyInfected * 2 ** (d.timeToElapse / 3);
-    const severeCasesByRequestedTime=infectionsByRequestedTime * 0.15;
-    const hospitalBedsByByRequestedTime=d.totalHospitalBeds * 0.35 - severeCasesByRequestedTime;
-    const casesForICUByRequestedTime=infectionsByRequestedTime * 0.05;
-    const casesForVentilatorsByRequestedTime=infectionsByRequestedTime * 0.02;
-    const dollarsInFlight =infectionsByRequestedTime * d.region.avgDailyIncomePopulation * d.region.avgDailyIncomeInUSD * d.timeToElapse;
+    const currentlyInfected = impact * d.reportedCases;
+    const infectionsByRequestedTime = currentlyInfected * 2 ** (d.timeToElapse / 3);
+    const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
+    const hospitalBedsByByRequestedTime = d.totalHospitalBeds * 0.35 - severeCasesByRequestedTime;
+    const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
+    const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
+    const dollarsInFlight = infectionsByRequestedTime * d.region.avgDailyIncomePopulation * d.region.avgDailyIncomeInUSD * d.timeToElapse;
 
     return {
       currentlyInfected,

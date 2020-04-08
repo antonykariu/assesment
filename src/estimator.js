cc3d12236c96f7d5f39997122dbab59aa12b5e7f
antonykariu <kariuantony@gmail.com>
2020-04-08 12:20:41 +0300

    error checking

--- src/estimator.js
+++ src/estimator.js
@@ -1,31 +1,37 @@
 const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const impacts = [10, 50];
+  const outcome = (data, impact) => {
+    const currentlyInfected = impact * data.reportedCases;
+    const infectionsByRequestedTime =
+      currentlyInfected * 2 ** (data.timeToElapse / 3);
+    const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
+    const hospitalBedsByByRequestedTime =
+      data.totalHospitalBeds * 0.35 - severeCasesByRequestedTime;
+    const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
+    const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
+    const dollarsInFlight =
+      infectionsByRequestedTime *
+      data.region.avgDailyIncomePopulation *
+      data.region.avgDailyIncomeInUSD *
+      data.timeToElapse;
 
-    impacts = [10,50];
-    const outcome = (data,impact) =>{
-        const currentlyInfected = impact * data.reportedCases;
-        const infectionsByRequestedTime = currentlyInfected * (2 ** (data.timeToElapse/3));
-        const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
-        const hospitalBedsByByRequestedTime = (data.totalHospitalBeds * 0.35) - severeCasesByRequestedTime;
-        const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
-        const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
-        const dollarsInFlight = (infectionsByRequestedTime * data.region.avgDailyIncomePopulation) * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+    return {
+      currentlyInfected,
+      infectionsByRequestedTime,
+      severeCasesByRequestedTime,
+      hospitalBedsByByRequestedTime,
+      casesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime,
+      dollarsInFlight
+    };
+  };
 
-        return {
-            currentlyInfected,
-            infectionsByRequestedTime,
-            severeCasesByRequestedTime,
-            hospitalBedsByByRequestedTime,
-            casesForICUByRequestedTime,
-            casesForVentilatorsByRequestedTime,
-            dollarsInFlight
-        }
-    }    
-
-    return{
-        data,
-        impact: outcome(data, impacts[0]),
-        severeImpact: outcome(data, impacts[1])
-    }
-}
+  return {
+    data: input,
+    impact: outcome(input, impacts[0]),
+    severeImpact: outcome(input, impacts[1])
+  };
+};
 
 export default covid19ImpactEstimator;

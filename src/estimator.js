7220e05d974b6503d4e9db5faf5cac18b52c23b2
antonykariu <kariuantony@gmail.com>
2020-04-08 12:10:55 +0300

    challenges 1,2 and 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+
+    impacts = [10,50];
+    const outcome = (data,impact) =>{
+        const currentlyInfected = impact * data.reportedCases;
+        const infectionsByRequestedTime = currentlyInfected * (2 ** (data.timeToElapse/3));
+        const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
+        const hospitalBedsByByRequestedTime = (data.totalHospitalBeds * 0.35) - severeCasesByRequestedTime;
+        const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
+        const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
+        const dollarsInFlight = (infectionsByRequestedTime * data.region.avgDailyIncomePopulation) * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+
+        return {
+            currentlyInfected,
+            infectionsByRequestedTime,
+            severeCasesByRequestedTime,
+            hospitalBedsByByRequestedTime,
+            casesForICUByRequestedTime,
+            casesForVentilatorsByRequestedTime,
+            dollarsInFlight
+        }
+    }    
+
+    return{
+        data,
+        impact: outcome(data, impacts[0]),
+        severeImpact: outcome(data, impacts[1])
+    }
+}
 
 export default covid19ImpactEstimator;

d4bda9725d197236c0f23304f31ea108961026ba
antonykariu <kariuantony@gmail.com>
2020-04-13 14:07:32 +0300

    challenges 1,2 and 3 revisions 4

--- src/estimator.js
+++ src/estimator.js
@@ -21,8 +21,8 @@ const covid19ImpactEstimator = (data) => {
     const infectionsByRequestedTime = currentlyInfected * (2 ** (timeInDays / 3));
     const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
     const hospitalBedsByRequestedTime = Math.trunc(d.totalHospitalBeds * 0.35 - severeCasesByRequestedTime);
-    const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
-    const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
+    const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
+    const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
     const dollarsInFlight = infectionsByRequestedTime * d.region.avgDailyIncomePopulation
     * d.region.avgDailyIncomeInUSD * timeInDays;
 
